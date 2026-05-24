ae9756f062805113b90744ce90d9c478013c9ffa
Bolol4002 <sayoojsumesh1112@gmail.com>
2026-05-24 09:36:44 +0530

    pointer dept logic

--- rtl/fifo.v
+++ rtl/fifo.v
@@ -10,4 +10,15 @@ module fifo #(
     output wire full,
     output wire empty
 );
+    reg [WIDTH-1:0] mem [0:DEPTH-1];
+
+    //$clog2(N) means:
+    //Ceiling of log₂(N)
+    //Translation:
+    //Minimum number of bits needed to represent N states.
+
+    localparam ptr_width = $clog2(DEPTH);
+    reg [ptr_width-1:0] wptr;
+    reg [ptr_width-1:0] rptr;
+    reg [ptr_width-1:0] count;  
 endmodule
